5ff1c0e1aa9cc3c2c4768ac40b6a359f09dfd25b
PaNDa2Code <moaaz0688@gmail.com>
2025-01-12 08:52:37 +0200

    improved rwlock

--- include/rwlock.h
+++ include/rwlock.h
@@ -1,16 +1,24 @@
 #pragma once
 
+#include <limits.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include <stdint.h>
 
+#define IS_WRITING ((size_t)1)
+#define WRITER ((size_t)1 << 1)
+#define Count ((sizeof(size_t) * CHAR_BIT - 1) / 2)
+#define READER ((size_t)(1ULL << (1 + Count)))
+#define WRITER_MASK ((size_t)((~(size_t)0) << 2))
+#define READER_MASK ((size_t)((~(size_t)0) << (1 + Count)))
+
 typedef struct _rwlock {
-  pthread_mutex_t w_mutex;
-  pthread_mutex_t r_mutex;
-  pthread_mutex_t rw_mutex;
-  uint32_t readers;
+  _Atomic size_t state;
+  pthread_mutex_t mutex;
+  sem_t semaphore;
 } rwlock_t;
 
-#define RWLOCK_INITIALIZER {PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER, 0}
+#define RWLOCK_INITIALIZER {0, PTHREAD_MUTEX_INITIALIZER, {0}}
 
 void r_lock(rwlock_t *rwlock);
 void r_unlock(rwlock_t *rwlock);
--- src/rwlock.c
+++ src/rwlock.c
@@ -1,32 +1,52 @@
 #include "rwlock.h"
 #include <pthread.h>
+#include <stdatomic.h>
+
+#include "rwlock.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void r_lock(rwlock_t *rwlock) {
-  pthread_mutex_lock(&rwlock->w_mutex); // Wait for writers to finish
-  pthread_mutex_lock(&rwlock->r_mutex);
-  rwlock->readers++;
-  if (rwlock->readers == 1)
-    pthread_mutex_lock(&rwlock->rw_mutex); // First reader locks resource
+    size_t state = atomic_load_explicit(&rwlock->state, memory_order_acquire);
 
-  pthread_mutex_unlock(&rwlock->r_mutex);
-  pthread_mutex_unlock(&rwlock->w_mutex);
+    while (1) {
+        if (state & (IS_WRITING | WRITER_MASK)) {
+            pthread_mutex_lock(&rwlock->mutex);
+            pthread_mutex_unlock(&rwlock->mutex);
+            state = atomic_load_explicit(&rwlock->state, memory_order_acquire);
+            continue;
+        }
+
+        if (atomic_compare_exchange_weak_explicit(
+                &rwlock->state, &state, state + READER,
+                memory_order_acquire, memory_order_relaxed)) {
+            break;
+        }
+    }
 }
 
 void r_unlock(rwlock_t *rwlock) {
-  pthread_mutex_lock(&rwlock->r_mutex);
-  rwlock->readers--;
-  if (rwlock->readers == 0) {
-    pthread_mutex_unlock(&rwlock->rw_mutex); // Last reader unlocks resource
-  }
-  pthread_mutex_unlock(&rwlock->r_mutex);
+    size_t state = atomic_fetch_sub_explicit(&rwlock->state, READER, memory_order_release);
+
+    if ((state & READER_MASK) == READER && (state & IS_WRITING)) {
+        sem_post(&rwlock->semaphore);
+    }
 }
 
 void w_lock(rwlock_t *rwlock) {
-  pthread_mutex_lock(&rwlock->w_mutex);
-  pthread_mutex_lock(&rwlock->rw_mutex);
+    atomic_fetch_add_explicit(&rwlock->state, WRITER, memory_order_acquire);
+
+    pthread_mutex_lock(&rwlock->mutex);
+
+    size_t state = atomic_fetch_add_explicit(&rwlock->state, IS_WRITING - WRITER, memory_order_acquire);
+
+    if (state & READER_MASK) {
+        sem_wait(&rwlock->semaphore);
+    }
 }
 
 void w_unlock(rwlock_t *rwlock) {
-  pthread_mutex_unlock(&rwlock->rw_mutex);
-  pthread_mutex_unlock(&rwlock->w_mutex);
+    atomic_fetch_and_explicit(&rwlock->state, ~IS_WRITING, memory_order_release);
+
+    pthread_mutex_unlock(&rwlock->mutex);
 }
